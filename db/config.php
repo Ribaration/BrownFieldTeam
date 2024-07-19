7264753d81d4b98a26b8e7a2f03d0c62a1873d6b
Lerato Zelda Riba <152501692+Ribaration@users.noreply.github.com>
2024-07-19 11:05:55 +0200

    Update config.php
    Connected the files to the cloud database

--- db/config.php
+++ db/config.php
@@ -1,8 +1,8 @@
 <?php
-DEFINE('DBHost','127.0.0.1');
-DEFINE('DBUser', 'root');
-DEFINE('DBPass','');
-DEFINE('DBName','E-Insurance');
+DEFINE('DBHost','sql.freedb.tech');
+DEFINE('DBUser', 'freedb_BrownFieldTeam_user');
+DEFINE('DBPass','fw@%5?kXHCQV?XX');
+DEFINE('DBName','freedb_BrownFieldTeam_db');
 DEFINE('DBCharset','utf8mb4');
 DEFINE('DBCollation', 'utf8_general_ci');
 DEFINE('DBPrefix', '');
